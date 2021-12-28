8cab9a65bf83d2efaaa4ce815f046f416b8ad787
unknown <yuvicivil849@gmail.com>
2021-12-28 23:04:23 +0530

    testpro

---
+++ src/org/pc/Versus.java
@@ -0,0 +1,20 @@
+package org.pc;
+ 
+public class Versus {
+	
+	   public  void emp() {
+		   System.out.println("result");	
+	}
+	   private static void voi() {
+		   
+          System.out.println("res");
+	}
+	   public static void main(String[] args) {
+		  Versus h = new Versus();
+		  h.emp();
+		  voi();
+		
+	}
+	
+	}
+
